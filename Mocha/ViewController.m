b5b3645163e55ec75ee24714e62fc37181cafd6a
kccarbone <kcarbone@buffalo.edu>
2014-04-28 21:52:55 -0400

    playing around

--- Mocha/ViewController.m
+++ Mocha/ViewController.m
@@ -39,10 +39,9 @@
         self.vwLoading.transform = CGAffineTransformMakeScale(1, 1);
         self.vwLoading.alpha = 1;
     } completion:^(BOOL finished){
-        [UIView animateWithDuration:1.3 delay:0.8 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        [UIView animateWithDuration:.3 delay:0.8 options:UIViewAnimationOptionCurveEaseOut animations:^{
             //self.vwLoading.transform = CGAffineTransformMakeScale(0, 0);
-            //self.vwLoading.alpha = 0;
-            self.view.transform = CGAffineTransformMakeScale(0.5, 0.5);
+            self.vwLoading.alpha = 0;
         } completion:^(BOOL finished2){
             [self dismissViewControllerAnimated:YES completion:nil];
         }];
